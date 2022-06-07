c5d15e7bad24f4c9e512aceb356c5322f6ad9a59
liu <liu>
2022-06-07 17:56:05 +0800

    update

---
+++ .solcover.js
@@ -0,0 +1,13 @@
+module.exports = {
+    skipFiles: [
+        'mock/mockLib.sol',
+        'mock/mockERC20.sol',
+        'mock/mockERC20.sol',
+        "mock/mockRelayerRegistry.sol",
+        "mock/mockSwap.sol",
+        "mock/mockTornadoGovernanceStaking.sol",
+        "mock/mockTornadoStakingRewards.sol",
+        "mock/mockTornRouter.sol",
+        "mock/interface/ImockTronRouter.sol",
+    ]
+};
